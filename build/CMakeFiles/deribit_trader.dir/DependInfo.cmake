2e94fadd8227dda2228069ae4e5e424c1294032c
Shahil Routh <routhshahil@gmail.com>
2025-07-24 01:54:27 -0700

    Initial commit

---
+++ build/CMakeFiles/deribit_trader.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/root/deribit-trading-system/src/api/api.cpp" "CMakeFiles/deribit_trader.dir/src/api/api.cpp.o" "gcc" "CMakeFiles/deribit_trader.dir/src/api/api.cpp.o.d"
+  "/root/deribit-trading-system/src/authentication/password.cpp" "CMakeFiles/deribit_trader.dir/src/authentication/password.cpp.o" "gcc" "CMakeFiles/deribit_trader.dir/src/authentication/password.cpp.o.d"
+  "/root/deribit-trading-system/src/latency/tracker.cpp" "CMakeFiles/deribit_trader.dir/src/latency/tracker.cpp.o" "gcc" "CMakeFiles/deribit_trader.dir/src/latency/tracker.cpp.o.d"
+  "/root/deribit-trading-system/src/main.cpp" "CMakeFiles/deribit_trader.dir/src/main.cpp.o" "gcc" "CMakeFiles/deribit_trader.dir/src/main.cpp.o.d"
+  "/root/deribit-trading-system/src/utils/utils.cpp" "CMakeFiles/deribit_trader.dir/src/utils/utils.cpp.o" "gcc" "CMakeFiles/deribit_trader.dir/src/utils/utils.cpp.o.d"
+  "/root/deribit-trading-system/src/websocket/websocket_client.cpp" "CMakeFiles/deribit_trader.dir/src/websocket/websocket_client.cpp.o" "gcc" "CMakeFiles/deribit_trader.dir/src/websocket/websocket_client.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/deribit_trader.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/deribit_trader.dir/src/api/api.cpp.o"
+  "CMakeFiles/deribit_trader.dir/src/api/api.cpp.o.d"
+  "CMakeFiles/deribit_trader.dir/src/authentication/password.cpp.o"
+  "CMakeFiles/deribit_trader.dir/src/authentication/password.cpp.o.d"
+  "CMakeFiles/deribit_trader.dir/src/latency/tracker.cpp.o"
+  "CMakeFiles/deribit_trader.dir/src/latency/tracker.cpp.o.d"
+  "CMakeFiles/deribit_trader.dir/src/main.cpp.o"
+  "CMakeFiles/deribit_trader.dir/src/main.cpp.o.d"
+  "CMakeFiles/deribit_trader.dir/src/utils/utils.cpp.o"
+  "CMakeFiles/deribit_trader.dir/src/utils/utils.cpp.o.d"
+  "CMakeFiles/deribit_trader.dir/src/websocket/websocket_client.cpp.o"
+  "CMakeFiles/deribit_trader.dir/src/websocket/websocket_client.cpp.o.d"
+  "deribit_trader"
+  "deribit_trader.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/deribit_trader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
